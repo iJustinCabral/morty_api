30bfd76c85699fad7d3d5c418c3b1be308a614c1
Justin Cabral <ijustincabral@gmail.com>
2022-12-27 10:48:45 -0500

    Initial Commit

---
+++ RickAndMortyAPI/RickAndMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIApp.swift
+//  RickAndMortyAPI
+//
+//  Created by Justin Cabral on 12/27/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
